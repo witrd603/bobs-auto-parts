e14bcab43b2538a9daf234e80cdfe3a7b97fb275
ypegasi <ypegasi@hotmail.com>
2017-06-09 09:58:06 +0200

    Initial commit

---
+++ processorder.php
@@ -0,0 +1,36 @@
+<?php
+//create short variable names
+$tireqty = $_POST['tireqty'];
+$oilqty = $_POST['oilqty'];
+$sparkqty = $_POST['sparkqty'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bob's Auto Parts - Order Results</title>
+</head>
+<body>
+  <h1>Bob's Auto Parts</h1>
+  <h2>Order Results</h2>
+  <?php
+echo "<p>Order processed at ".date( 'H:i, jS F Y'). "</p>";
+
+echo '<p>  Your order is as follows:  </p>';
+echo htmlspecialchars($tireqty).' tires<br />';
+echo htmlspecialchars($oilqty).' bottles of oil<br />';
+echo htmlspecialchars($sparkqty).' spark plugs<br />';
+
+$totalqty = 0;
+$totalamount = $totalqty;
+
+define('TIREPRICE', 10);
+define('OILPRICE', 10);
+define ('SPARKPRICE', 4);
+
+echo TIREPRICE;
+
+phpinfo()
+
+?>
+</body>
+</html>
